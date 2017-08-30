56da365683d799b2dbf2c826c6e843b58d415c8a
d-smith <doug.smith.mail@gmail.com>
2017-08-30 13:14:17 -0700

    updated code to product queue context object

--- s2fq/stream2fifo.js
+++ s2fq/stream2fifo.js
@@ -1,3 +1,12 @@
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+
+
 exports.handler = (event, context, callback) => {
     console.log('event');
     console.log(event);
@@ -7,6 +16,15 @@ exports.handler = (event, context, callback) => {
     for (let record of event.Records) {
         console.log(record.dynamodb.Keys);
         console.log(record.dynamodb.NewImage);
+
+        let ddbCtx = {};
+        ddbCtx.timestamp = Date.now();
+        ddbCtx.opcode = record.eventName;
+        ddbCtx.keys = record.dynamodb.Keys;
+        ddbCtx.newImage = record.dynamodb.NewImage;
+        ddbCtx.writeId = uuidv4();
+
+        console.log(ddbCtx)
     }
 
     callback(null, 'yeah ok');
